084e4bb6f7e3cf447d72735ded270684b8c74b8e
ayatekapoetra <ayatekapoetra@gmail.com>
2025-10-09 12:32:09 +0800

    chore: update footer branding to MakkuragaTama Dashboard

--- src/layout/DashboardLayout/Footer.js
+++ src/layout/DashboardLayout/Footer.js
@@ -1,24 +1,54 @@
 // NEXT
-import Link from 'next/link';
+import Link from "next/link";
 
 // MATERIAL - UI
-import Links from '@mui/material/Link';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
+import Links from "@mui/material/Link";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 
 // ==============================|| MAIN LAYOUT - FOOTER ||============================== //
 
 const Footer = () => (
-  <Stack direction="row" justifyContent="space-between" alignItems="center" sx={{ p: '24px 16px 0px', mt: 'auto' }}>
-    <Typography variant="caption">&copy; Able Pro ♥ crafted by Team Phoenixcoded</Typography>
-    <Stack spacing={1.5} direction="row" justifyContent="space-between" alignItems="center">
-      <Links component={Link} href="https://ableproadmin.com" target="_blank" variant="caption" color="textPrimary">
+  <Stack
+    direction="row"
+    justifyContent="space-between"
+    alignItems="center"
+    sx={{ p: "24px 16px 0px", mt: "auto" }}
+  >
+    <Typography variant="caption">
+      MakkuragaTama Dashboard Versi 1.0.1
+    </Typography>
+    <Stack
+      spacing={1.5}
+      direction="row"
+      justifyContent="space-between"
+      alignItems="center"
+    >
+      <Links
+        component={Link}
+        href="https://makkuragatama.id"
+        target="_blank"
+        variant="caption"
+        color="textPrimary"
+      >
         Home
       </Links>
-      <Links component={Link} href="https://phoenixcoded.gitbook.io/able-pro/v/react" target="_blank" variant="caption" color="textPrimary">
+      <Links
+        component={Link}
+        href="https://makkuragatama.id/docs"
+        target="_blank"
+        variant="caption"
+        color="textPrimary"
+      >
         Documentation
       </Links>
-      <Links component={Link} href="https://phoenixcoded.authordesk.app/" target="_blank" variant="caption" color="textPrimary">
+      <Links
+        component={Link}
+        href="https://makkuragatama.id/support"
+        target="_blank"
+        variant="caption"
+        color="textPrimary"
+      >
         Support
       </Links>
     </Stack>
