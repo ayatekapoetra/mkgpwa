c027cc93960d87cef1148cec9e060d55c5917693
ayatekapoetra <ayatekapoetra@gmail.com>
2025-10-09 18:38:11 +0800

    chore: bump to v1.1.5

--- src/layout/DashboardLayout/Footer.js
+++ src/layout/DashboardLayout/Footer.js
@@ -24,7 +24,7 @@ const Footer = () => (
         MakkuragaTama Dashboard
       </Typography>
       <Chip
-        label="v1.1.4"
+        label="v1.1.5"
         size="small"
         color="success"
         variant="filled"
