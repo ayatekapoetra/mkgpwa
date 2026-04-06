53c338292104a9167a02b82b996c02764629cdcc
ayatekapoetra <ayatekapoetra@gmail.com>
2026-04-07 02:56:36 +0800

    chore(timesheet-reconcil): add dashboard route alias for reconcil page

---
+++ src/app/(dashboard)/timesheet-reconcil/page.js
@@ -0,0 +1,7 @@
+import TimesheetReconcil from 'views/operational/timesheet-reconcil';
+
+const TimesheetReconcilPage = () => {
+  return <TimesheetReconcil />;
+};
+
+export default TimesheetReconcilPage;
